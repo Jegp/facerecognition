a7734b05f43a4b6d566b31126d14ad60dc485aa9
Jens Egholm <jensegholm@protonmail.com>
2017-10-03 17:29:51 +0200

    Added the experiment data, experiment sequence and experiment labels

--- randomize.sh
+++ randomize.sh
@@ -8,6 +8,8 @@ FILES=`ls -d *corrected/*.jpg | shuf --random-source=/dev/urandom`
 i=0
 for f in $FILES; do
 	cp "$f" "experiment/$i.jpg"
+	DIRNAME=`dirname $f`
+	echo "$i $DIRNAME" >> experiment_labels.txt
 	((i++))
 done
 
