23bc9ed0e0221e236491c154c5bd27cd835e3aa1
Jens Egholm <jensegholm@protonmail.com>
2017-10-03 16:51:14 +0200

    Added shell script for randomizing pictures

---
+++ randomize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##
+## Randomizes corrected images
+##
+
+FILES=`ls -d *corrected/*.jpg | shuf --random-source=/dev/urandom`
+i=0
+for f in $FILES; do
+	cp "$f" "experiment/$i.jpg"
+	((i++))
+done
+
