8fa5c8a9e786848f61355efa2e50bdf7c24b3af4
Jens Egholm <jensegholm@protonmail.com>
2017-11-17 16:47:55 +0100

    Replaced models with a single file using argument inputs

--- model.py
+++ model.py
@@ -9,9 +9,26 @@ from hyperas import optim
 from hyperas.distributions import choice, uniform, conditional
 from hyperopt import Trials, STATUS_OK, tpe
 
+import os
+import argparse
+import json
+import random
+
 def create_model(x_train, y_train, x_test, y_test):
+
+    def load_y_index_and_classes():
+        with open("args.dat") as d:
+            y_value = json.load(d)["y"]
+        if y_value == "binary":
+            return (4, 1)
+        elif y_value == "binaryno3":
+            return (5, 1)
+        elif y_value == "linear":
+            return (3, 5)
+        else:
+            return "Unknown predictor variable"
     data_dim = 1
-    num_classes = 5
+    num_classes = load_y_index_and_classes()[1]
 
     # Expected input shape: (batch_size, data_dim)
     # Note that we have to provide the full batch_input_shape since the network is stateful.
@@ -53,12 +70,31 @@ def create_model(x_train, y_train, x_test, y_test):
     return {'loss': -acc, 'status': STATUS_OK, 'model': model}
 
 
-# In[15]:
-
-import json
-import random
-
 def data():
+    def load_x_indices():
+        with open("args.dat") as d:
+            x_value = json.load(d)["X"]
+        if x_value == "fixations":
+            return [0]
+        elif x_value == "xy":
+            return [1]
+        elif x_value == "fixationsxy":
+            return [0, 1]
+        else:
+            return "Unknown input variables"
+
+    def load_y_index_and_classes():
+        with open("args.dat") as d:
+            y_value = json.load(d)["y"]
+        if y_value == "binary":
+            return (4, 2)
+        elif y_value == "binaryno3":
+            return (5, 2)
+        elif y_value == "linear":
+            return (3, 5)
+        else:
+            return "Unknown predictor variable"
+
     # Set the seed to achieve same sorting
     random.seed(6781693213)
 
@@ -68,18 +104,27 @@ def data():
     ids_train = ids[:80]
     ids_test = ids [80:]
 
-    x_train = []
-    y_train = []
-    x_test = []
-    y_test = []
+    x_data_indices = load_x_indices()
+    y_data_index = load_y_index_and_classes()[0]
 
     with open("modified_data.txt") as data:
         rows = json.load(data)
 
-        x_train = np.take(rows[0], ids_train).reshape(-1, 1, 1)
-        x_test = np.take(rows[0], ids_test).reshape(-1, 1, 1)
-        y_train = (np.take(rows[2], ids_train) - 1).reshape(-1, 1)
-        y_test = (np.take(rows[2], ids_test) - 1).reshape(-1, 1)
+        # Remove NAN values if 3 is removed
+        if y_data_index == 5:
+            ids_train = [x for x in rows[y_data_index] if not x == None]
+            ids_test = [x for x in rows[y_data_index] if not x == None]
+
+        if len(x_data_indices) == 2:
+            x_train = np.take(rows[x_data_indices[0]:x_data_indices[1]], ids_train).reshape(-1, 1, 1)
+            x_test = np.take(rows[x_data_indices[0]:x_data_indices[1]], ids_test).reshape(-1, 1, 1)
+            y_train = (np.take(rows[y_data_index], ids_train) - 1).reshape(-1, 1)
+            y_test = (np.take(rows[y_data_index], ids_test) - 1).reshape(-1, 1)
+        else:
+            x_train = np.take(rows[x_data_indices[0]], ids_train).reshape(-1, 1, 1)
+            x_test = np.take(rows[x_data_indices[0]], ids_test).reshape(-1, 1, 1)
+            y_train = (np.take(rows[y_data_index], ids_train) - 1).reshape(-1, 1)
+            y_test = (np.take(rows[y_data_index], ids_test) - 1).reshape(-1, 1)
 
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
@@ -88,11 +133,23 @@ def data():
 
 # In[16]:
 
-
 if __name__ == '__main__':
+    global x_data_indices, y_data_index, num_classes
+    parser = argparse.ArgumentParser(description='Run facial recognition model.')
+    parser.add_argument('X', metavar='X', type=str,
+                       help='the input data; either fixations, xy or fixationsxy')
+    parser.add_argument('y', metavar='y', type=str,
+                       help='the output data; either binary, binaryno3, linear')
+
+    args = parser.parse_args()
+
+    with open("args.dat", "w") as d:
+        json.dump({"X":args.X,"y":args.y}, d)
+
     best_run, best_model = optim.minimize(model=create_model,
                                           data=data,
                                           algo=tpe.suggest,
                                           max_evals=100,
                                           trials=Trials())
 print(best_run)
+os.remove("args.dat")
